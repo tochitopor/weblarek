b0105b111d42525f5cbe9fa4aabb6f8ed1457cbc
tochitopor <abichaika@yandex.ru>
2026-05-01 20:20:45 +0300

    добавил интерфесы IProduct и IBuyer

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,23 @@
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
+export type TPayment = 'card' | 'cash' | '';
+
 export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+}
+
+export interface IProduct {
+    id: string;
+    description: string;
+    image: string;
+    title: string;
+    price: number | null;
+}
+
+export interface IBuyer {
+    payment: TPayment;
+    email: string;
+    phone: string;
+    address: string;
 }
